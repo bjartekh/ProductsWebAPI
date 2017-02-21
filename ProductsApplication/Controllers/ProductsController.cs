75fbde0a0752631e70c64c9035dd4dcaec6da86d
bjartekh <bjartekhelland@gmail.com>
2017-02-21 23:51:19 +0100

    added features

---
+++ ProductsApplication/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsApplication.Models;
+
+namespace ProductsApplication.Controllers
+{
+    public class ProductsController : ApiController
+    {
+
+        Product[] products = new Product[]
+        {
+            new Product { Id=1, Name="Tomato Soup", Category="Groceries",  Price= 1 },
+            new Product { Id=2, Name="Yo-yo", Category="Toys", Price=2 },
+            new Product { Id=3, Name="Hammer", Category="Hardware", Price=2.5M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+
+    }
+
+}
---
+++ ProductsApplication/Controllers/UploadController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace ProductsApplication.Controllers
+{
+    public class UploadController : ApiController
+    {
+        public async Task<HttpResponseMessage> PostFormData()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    Debug.WriteLine(file.Headers.ContentDisposition.FileName);
+                    Debug.WriteLine("Server file path: " + file.LocalFileName);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+
+        }
+    }
+}
