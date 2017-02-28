d961591d709eb13a4e6d2fd3456f115656363990
bjartekh <bjartekhelland@gmail.com>
2017-02-28 02:29:25 +0100

    testing

---
+++ ProductsApplication/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿public struct Customer
+{
+    public int ID;
+    public string Name;
+
+    public Customer(int customerID, string customerName)
+    {
+        ID = customerID;
+        Name = customerName;
+    }
+}
---
+++ ProductsApplication/Reference/FileOperation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+/**
+ * From developer console: execute csc FileOperation (to compile), then run
+ */
+namespace ProductsApplication.Reference
+{
+    public class FileOperation
+    {
+
+        static void Main()
+        {
+            string fileName = @"e:\temp\test_file.txt";
+
+            if (System.IO.File.Exists(fileName))
+            {
+                System.IO.File.Delete(fileName);
+
+            }
+
+            using (System.IO.FileStream fs = System.IO.File.Create(fileName, 1024))
+            {
+                byte[] info = new System.Text.UTF8Encoding(true).GetBytes("ProductFileTest");
+                fs.Write(info, 0, info.Length);
+            }
+
+            using (System.IO.StreamReader sr = System.IO.File.OpenText(fileName))
+            {
+                string someText = "";
+                while ((someText = sr.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(someText);
+                }
+            }
+
+
+        }
+    }
+}
